645494957b3f9a31d6d0f74878c484866828b67f
HouseOfGhosts <70688776+Ghozta1320@users.noreply.github.com>
2025-02-10 17:14:24 -0500

    Add files via upload

---
+++ P1HW1_DELPIT_AVERY.py
@@ -0,0 +1,21 @@
+# AVERY DELPIT 
+# 02/10/2024
+# P1HW1 Homework
+
+print('-----Calculating Exponents-----')
+
+value = int(input('Enter an integer as the base value: '))
+exponent = int(input('Enter an integer as the exponent: '))
+result = value ** exponent
+
+print(value, 'raised to the power of', exponent, 'is', result)
+
+
+print('-----Addition and Subtraction-----')
+
+value_2 = int(input('Enter a starting integer: '))
+value_3 = int(input('Enter a second integer to add to the first: '))
+value_4 = int(input('Enter a third integer to subtract from the sum: '))
+result_2 = value_2 + value_3 - value_4
+
+print(value_2, '+', value_3, '-', value_4, 'is equal to', result_2)
